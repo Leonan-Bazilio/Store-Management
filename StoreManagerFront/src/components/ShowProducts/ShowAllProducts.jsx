7188e78747e04842aeaf29725bba0bf7a29320f3
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2024-12-30 22:30:46 -0300

    modificaçoes em ShowAllProducts

--- StoreManagerFront/src/components/ShowProducts/ShowAllProducts.jsx
+++ StoreManagerFront/src/components/ShowProducts/ShowAllProducts.jsx
@@ -1,45 +1,47 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styles from "./ShowAllProducts.module.css";
-
+import InputField from "../InputField/InputField";
 const ShowAllProducts = () => {
   const [products, setProducts] = useState([]);
-  const [filteredProducts, setFilteredProducts] = useState([]);
   const [fetching, setFetching] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
   const baseUrl = import.meta.env.VITE_BASE_URL;
 
+  const truncateDescription = (description, maxLength) => {
+    if (description.length > maxLength) {
+      return description.slice(0, maxLength) + "...";
+    }
+    return description;
+  };
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm)
+  );
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value.toLowerCase());
+  };
+
   useEffect(() => {
+    const fetchProducts = async () => {
+      setFetching(true);
+      try {
+        const response = await axios.get(`${baseUrl}/api/products`);
+        setProducts(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+      } finally {
+        setFetching(false);
+      }
+    };
+
     fetchProducts();
   }, []);
-
-  const fetchProducts = async () => {
-    setFetching(true);
-    try {
-      const response = await axios.get(`${baseUrl}/api/products`);
-      setProducts(response.data);
-      setFilteredProducts(response.data);
-    } catch (error) {
-      console.error("Erro ao buscar produtos:", error);
-    } finally {
-      setFetching(false);
-    }
-  };
-
-  const handleSearch = (e) => {
-    const term = e.target.value.toLowerCase();
-    setSearchTerm(term);
-    const filtered = products.filter((product) =>
-      product.name.toLowerCase().includes(term)
-    );
-    setFilteredProducts(filtered);
-  };
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Produtos Cadastrados</h1>
-      <input
+      <InputField
         type="text"
         placeholder="Pesquisar por nome do produto..."
         value={searchTerm}
@@ -58,8 +60,10 @@ const ShowAllProducts = () => {
                 className={styles.productImage}
               />
               <div className={styles.productInfo}>
-                <h4 className={styles.productName}>{prod.name}</h4>
-                <p className={styles.productDescription}>{prod.description}</p>
+                <h3 className={styles.productName}>{prod.name}</h3>
+                <p className={styles.productDescription}>
+                  {truncateDescription(prod.description, 40)}
+                </p>
                 <p className={styles.productPrice}>
                   <strong>Preço:</strong> R$ {prod.sellingPrice.toFixed(2)}
                 </p>
