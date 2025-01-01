e7096715998a31845314d290885f4b3c08483c7d
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2025-01-01 20:50:19 -0300

    modificaçoes ProductDetails

--- StoreManagerFront/src/components/ProductDetails/ProductDetails.jsx
+++ StoreManagerFront/src/components/ProductDetails/ProductDetails.jsx
@@ -3,10 +3,59 @@ import styles from "./ProductDetails.module.css";
 import InputField from "../InputField/InputField";
 
 const ProductDetails = ({ product }) => {
+  const baseUrl = import.meta.env.VITE_BASE_URL;
   return (
     <div className={styles.container}>
       <div className={styles.content}>
-        <h1>{product.name}</h1>
+        <div className={styles.row1}>
+          <img
+            className={styles.image}
+            src={`${baseUrl}/uploads/${product.imagePath}`}
+            alt={product.name}
+          />
+
+          <div className={styles.textContent}>
+            <h2>{product.name}</h2>
+            <p>{product.description}</p>
+          </div>
+        </div>
+        <div className={styles.row2}>
+          <div className={styles.divField}>
+            <p>Preço de custo: </p>
+
+            <p>R$ {product.costPrice},00</p>
+          </div>
+          <div className={styles.divField}>
+            <p>Preço de venda </p>
+            <p>R$ {product.sellingPrice},00</p>
+          </div>
+        </div>
+        <div className={styles.row3}>
+          <div className={styles.stockSection}>
+            <div className={styles.divField}>
+              <p>Quantidade em Estoque: </p>
+              <p> {product.stockQuantity}</p>
+            </div>
+            <div className={styles.addStock}>
+              <InputField
+                type="number"
+                nameAndId="stockQuantity"
+                placeholder="Adicionar ao estoque"
+                value={"depois adiciono"}
+                className={styles.inputStock}
+              />
+              <button className={styles.addButton}>Adicionar</button>
+            </div>
+          </div>
+          <div>
+            <p>Estoque minimo recomendado:</p>
+            <p>{product.intermediateWarningQuantity}</p>
+          </div>
+          <div>
+            <p>Estoque alerta: </p>
+            <p>{product.alertQuantity}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
