1811ac0583ce156a941bf40811b8cac84717c9de
LEONAN BAZILIO <leonanbaziliodev@gmail.com>
2025-01-02 22:01:56 -0300

    pequenas alteraçoes no inputField

--- StoreManagerFront/src/components/InputField/InputField.jsx
+++ StoreManagerFront/src/components/InputField/InputField.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./InputField.module.css";
 
 const InputField = ({
@@ -8,16 +9,28 @@ const InputField = ({
   onChange,
   className = "",
 }) => {
+  const [focused, setFocused] = useState();
+  const handleFocus = () => {
+    setFocused(true);
+  };
   return type !== "textarea" ? (
-    <input
-      type={type}
-      name={nameAndId}
-      id={nameAndId}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      className={`${styles.input} ${className}`}
-    />
+    <div className={`${styles.divInput}`}>
+      <input
+        onFocus={handleFocus}
+        type={type}
+        name={nameAndId}
+        id={nameAndId}
+        value={value}
+        onChange={onChange}
+        className={`${styles.input} ${className}`}
+      />
+      <label
+        className={`${styles.label} ${focused ? styles.focused : ""}`}
+        htmlFor={nameAndId}
+      >
+        {placeholder}
+      </label>
+    </div>
   ) : (
     <textarea
       name={nameAndId}
@@ -27,7 +40,9 @@ const InputField = ({
       onChange={onChange}
       className={`${styles.input} ${className}`}
     ></textarea>
-  ); // basicamente gambiarra pois não queria fazer separado para textarea sendo que vou utilizar ele so uma vez( ou solução tecnica alternativa)
+  );
 };
 
 export default InputField;
+
+/*ajustar onbrur para voltar label ao centro quando nao tiver valor e ajustar div para ter um espaço no top a mais talvez o padding para posicionar a label da forma que quero */
